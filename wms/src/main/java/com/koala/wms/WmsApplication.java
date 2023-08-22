068772b3960fb815fa9e3caf5f2ecaa68f74de90
koala86 <nanbiao66@gmail.com>
2023-08-23 06:39:30 +0900

    test

--- wms/src/main/java/com/koala/wms/WmsApplication.java
+++ wms/src/main/java/com/koala/wms/WmsApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WmsApplication {
 
+	// test
 	public static void main(String[] args) {
 		SpringApplication.run(WmsApplication.class, args);
 	}
